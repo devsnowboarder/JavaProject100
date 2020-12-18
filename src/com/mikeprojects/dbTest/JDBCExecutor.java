681c932eee504255d7bc6ea751bf6d80c2cd2c65
Mike Lau <michael_snowboarder@yahoo.com>
2020-12-18 00:24:32 -0800

    Add files via upload

---
+++ src/com/mikeprojects/dbTest/JDBCExecutor.java
@@ -0,0 +1,33 @@
+package com.mikeprojects.dbTest;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExecutor {
+
+    public static void main(String... args) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("local","mikeinfo","postgres","postgre123");
+
+
+
+        try {
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet  = statement.executeQuery("SELECT * FROM friendinfo");
+
+
+            while(resultSet.next()) {
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                System.out.println(firstName+"  "+lastName);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
