ad45e55bf14f84b905c319679b1bd084a7fa2812
MICHAEL LAU <michaellau@MICHAELs-MacBook-Pro.local>
2021-01-03 20:49:00 -0800

    more interface

---
+++ src/com/mikeprojects/Interface/Animal.java
@@ -0,0 +1,18 @@
+package com.mikeprojects.Interface;
+
+public interface Animal {
+    public void animalSound(); // interface method (does not have a body)
+    public void sleep(); // interface method (does not have a body)
+}
+
+
+class Pig implements Animal {
+    public void animalSound() {
+        // The body of animalSound() is provided here
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep() {
+        // The body of sleep() is provided here
+        System.out.println("Zzz");
+    }
+}
